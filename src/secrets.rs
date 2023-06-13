9ffd60dbb7d027f22b3f4b5d51f372c4384fe2dc
Mitch Gildenberg <mitch@switchboard.xyz>
2023-06-13 12:50:32 -0400

    add secrets module

---
+++ src/secrets.rs
@@ -0,0 +1,39 @@
+use crate::*;
+use rsa::{RsaPublicKey, RsaPrivateKey};
+use rsa::pkcs8::ToPublicKey;
+use rand::rngs::OsRng;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use rsa::PaddingScheme;
+use serde_json;
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[derive(Debug, Deserialize)]
+struct Secrets {
+    mrEnclaves: Vec<String>,
+    permittedAdvisories: Vec<String>,
+    keys: HashMap<String, String>,
+}
+
+async fn fetch_secrets(url: &str) -> Secrets {
+    let mut os_rng = OsRng::default();
+    let priv_key = RsaPrivateKey::new(&mut os_rng, 2048).unwrap();
+    let pub_key = RsaPublicKey::from(&priv_key).to_public_key_der().unwrap();
+    let pub_key: &[u8] = pub_key.as_ref();
+    let secrets_quote = Sgx::gramine_generate_quote(pub_key).unwrap();
+    let client = reqwest::Client::new();
+    let res = client.post("http://20.254.56.236:8080")
+        .json(&json!({
+            "quote": &secrets_quote,
+            "pubkey": pub_key,
+        }))
+        .send()
+        .await
+        .unwrap();
+    let ciphertext = res.bytes().await.unwrap();
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    let secrets: Secrets = serde_json::from_slice(&priv_key.decrypt(padding, &ciphertext).unwrap()).unwrap();
+    secrets
+}
