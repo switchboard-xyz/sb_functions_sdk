495f83528dac7f7a5c09353b69c7e291915f4f4b
Mitch Gildenberg <mitch@switchboard.xyz>
2023-06-13 12:53:42 -0400

    add secrets module

--- src/secrets.rs
+++ src/secrets.rs
@@ -2,7 +2,7 @@ use crate::*;
 use rsa::{RsaPublicKey, RsaPrivateKey};
 use rsa::pkcs8::ToPublicKey;
 use rand::rngs::OsRng;
-use serde_json::{json, Value};
+use serde_json::{json};
 use std::collections::HashMap;
 use rsa::PaddingScheme;
 use serde_json;
@@ -11,20 +11,20 @@ use serde::Deserialize;
 #[allow(dead_code)]
 #[allow(non_snake_case)]
 #[derive(Debug, Deserialize)]
-struct Secrets {
+pub struct Secrets {
     mrEnclaves: Vec<String>,
     permittedAdvisories: Vec<String>,
     keys: HashMap<String, String>,
 }
 
-async fn fetch_secrets(url: &str) -> Secrets {
+pub async fn fetch_secrets(url: &str) -> Secrets {
     let mut os_rng = OsRng::default();
     let priv_key = RsaPrivateKey::new(&mut os_rng, 2048).unwrap();
     let pub_key = RsaPublicKey::from(&priv_key).to_public_key_der().unwrap();
     let pub_key: &[u8] = pub_key.as_ref();
     let secrets_quote = Sgx::gramine_generate_quote(pub_key).unwrap();
     let client = reqwest::Client::new();
-    let res = client.post("http://20.254.56.236:8080")
+    let res = client.post(url)
         .json(&json!({
             "quote": &secrets_quote,
             "pubkey": pub_key,
